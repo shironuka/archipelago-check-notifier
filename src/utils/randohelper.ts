8b7b330a2753e5a0c45064e8ba6e379f5aabce56
shironuka <shironuka93@gmail.com>
2026-03-20 21:21:10 -0400

    Update randohelper.ts
    Update to findPlayers, client.package and correct lookupItemName/Locationname

--- src/utils/randohelper.ts
+++ src/utils/randohelper.ts
@@ -1,38 +1,35 @@
-import { Client, GamePackage, ITEM_FLAGS } from 'archipelago.js'
+import { Client, itemClassifications } from 'archipelago.js'
 
 function getItem (client: Client, playerId: number, itemId: number, flag: number): string {
-  const game: string | undefined = client.players.findPlayer(playerId)?.game
-  if (game === undefined) return 'Unknown Item'
+  const game = client.players.findPlayer(playerId)?.game
+  if (game == null) return 'Unknown Item'
 
-  const dataPackage: GamePackage | undefined = client.data.package.get(game)
-  if (dataPackage === undefined) return 'Unknown Item'
+  const itemName = client.package.lookupItemName(game, itemId)
+  if (itemName == null) return 'Unknown Item'
 
-  const item = Object.entries(dataPackage.item_name_to_id).find(([, id]) => id === itemId)
-  if (item === undefined) return 'Unknown Item'
-
-  switch (flag) {
-    case ITEM_FLAGS.PROGRESSION:
-      return `**${item[0]}**`
-    case ITEM_FLAGS.NEVER_EXCLUDE:
-      return `*${item[0]}*`
-    case ITEM_FLAGS.TRAP:
-      return `~~${item[0]}~~`
-    default:
-      return item[0]
+  if ((flag & itemClassifications.progression) !== 0) {
+    return `**${itemName}**`
   }
+
+  if ((flag & itemClassifications.trap) !== 0) {
+    return `~~${itemName}~~`
+  }
+
+  if ((flag & itemClassifications.useful) !== 0) {
+    return `*${itemName}*`
+  }
+
+  return itemName
 }
 
-function getLocation (client: Client, playerId: number, locationId: number) {
-  const game: string | undefined = client.players.findPlayer(playerId)?.game
-  if (game === undefined) return 'Unknown Location'
+function getLocation (client: Client, playerId: number, locationId: number): string {
+  const game = client.players.findPlayer(playerId)?.game
+  if (game == null) return 'Unknown Location'
 
-  const dataPackage: GamePackage | undefined = client.data.package.get(game)
-  if (dataPackage === undefined) return 'Unknown Location'
+  const locationName = client.package.lookupLocationName(game, locationId)
+  if (locationName == null) return 'Unknown Location'
 
-  const location = Object.entries(dataPackage.location_name_to_id).find(([, id]) => id === locationId)
-  if (location === undefined) return 'Unknown Location'
-
-  return location[0]
+  return locationName
 }
 
 const RandomHelper = {
