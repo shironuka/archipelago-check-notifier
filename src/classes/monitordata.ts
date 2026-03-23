ee0a726c44bb916f3a89fca3962d244b269bb810
shironuka <shironuka93@gmail.com>
2026-03-23 03:01:20 +0000

    Adding more to monitoring data

--- src/classes/monitordata.ts
+++ src/classes/monitordata.ts
@@ -1,13 +1,30 @@
 export default class MonitorData {
-  id?: number
+  id: number
   host: string
   port: number
-  game?: string
   player: string
+  game?: string
   channel: string
+
+  // mention settings (optional but used elsewhere)
   mention_join_leave: boolean
   mention_item_finder: boolean
   mention_item_receiver: boolean
-  mention_completion: boolean
   mention_hints: boolean
+  mention_completion: boolean
+
+  constructor (data: Partial<MonitorData>) {
+    this.id = data.id ?? 0
+    this.host = data.host ?? 'archipelago.gg'
+    this.port = data.port ?? 0
+    this.player = data.player ?? ''
+    this.game = data.game
+    this.channel = data.channel ?? ''
+
+    this.mention_join_leave = data.mention_join_leave ?? true
+    this.mention_item_finder = data.mention_item_finder ?? true
+    this.mention_item_receiver = data.mention_item_receiver ?? true
+    this.mention_hints = data.mention_hints ?? true
+    this.mention_completion = data.mention_completion ?? true
+  }
 }
